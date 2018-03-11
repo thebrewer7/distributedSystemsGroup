f7d801bf16b4ce7ca2f07a65f20f6c42e5e21e36
kelliruddy <kelli.ruddy@yahoo.com>
2018-03-11 15:52:03 -0700

    Add files via upload

--- assignment2/TransactionalServer/src/transactionalserver/account/AccountManager.java
+++ assignment2/TransactionalServer/src/transactionalserver/account/AccountManager.java
@@ -9,12 +9,47 @@ package transactionalserver.account;
  * 
  *  This class will call the LockManager to acquire locks for reads & writes
  */
+package accountmanager;
+
+/**
+ *
+ * @author Kelli
+ */
 public class AccountManager {
     
-    /**
+    private ArrayList<Account> accounts;
+
+ /**
      * Constructor
      */
     public AccountManager(){
+        //create a new account
+        accounts = new ArrayList <Account>();
         
     }
+    /*
+    get the account balance of accounts
+    @param account id and transaction id
+    */
+    public int getBalance(int transactionID, int accountID){
+        //balance is the specific account ID's balance
+        int balance = account.getBalance();
+        
+        //call lock manager for READ and set a lock giving the current account id, transaction id
+        
+        return balance;
+    }
+    
+    /*
+    set the account balance
+    @param account id, transaction id, amount being added to balance
+    */
+    public int setBalance(){
+        //add the tranfer amount to the current balance
+        
+        //call lock manager for WRITE and set a lock giving the current account id, transaction id
+        
+        return balance;
+    }
+    
 }
