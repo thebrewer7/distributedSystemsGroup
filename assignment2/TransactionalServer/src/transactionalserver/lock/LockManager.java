093c5c35f008c2a03652c656777e780cadcbc5cc
kelliruddy <kelli.ruddy@yahoo.com>
2018-03-11 16:24:57 -0700

    Add files via upload

--- assignment2/TransactionalServer/src/transactionalserver/lock/LockManager.java
+++ assignment2/TransactionalServer/src/transactionalserver/lock/LockManager.java
@@ -8,10 +8,44 @@ package transactionalserver.lock;
  */
 public class LockManager {
     
-    /**
-     * Constructor
-     */
+    //Some kind of data structure to hold locks info
+    
     public LockManager(){
+        //initialize structure holding locks
         
+    }
+    
+    /**
+     * Set Locks
+     * @param account being acted on and specific transactionID 
+     */
+    public void setLock(Account account, int transactionID){
+        
+    }
+    
+    public void unlock(Account account, int transactionID){
+        
+    }
+    
+    /**
+     * Check for conflict before setting locks
+     * 1. Read already performed... do not allow anyone else to write 
+     *      until commit or abort
+     * 2. Write already performed... do not allow anyone else to 
+     *      read OR write until commit or abort
+     * @param account 
+     * @param transactionID
+     */
+    public boolean checkConflict(Account account, int transactionID){
+        if( "read".equals(lockType)){
+            //wait for a commit or abort
+            return false;
+        }
+        
+        if("write".equals(lockType)){
+            //wait for commit or abort
+            return false;
+        }
+        return true; 
     }
 }
