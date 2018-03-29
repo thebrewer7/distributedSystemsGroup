7c3fb029aaaa5e2d72dfbf635f9706c2e79b9001
kelliruddy <kelli.ruddy@yahoo.com>
2018-03-28 18:23:36 -0700

    update lock method

--- assignment2/TransactionalServer/src/transactionalserver/lock/LockManager.java
+++ assignment2/TransactionalServer/src/transactionalserver/lock/LockManager.java
@@ -1,12 +1,8 @@
 package transactionalserver.lock;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Properties;
 
 import transactionalserver.account.Account;
-import transactionalserver.transaction.Transaction;
         
 /**
  *  The LockManager handles and initializes all of the Lock objects
@@ -14,47 +10,51 @@ import transactionalserver.transaction.Transaction;
  *  It will acquire and release locks for the AccountManager.
  *  Unlocking will be done in the TransactionsMaangerWorker.
  */
-public class LockManager{
-    private boolean applyLocking;
+public class LockManager implements LockType{
+    
     //Some kind of data structure to hold locks info
     private HashMap<Account, Lock> locks;
-    
 
     public LockManager(String clientProperties){
-        // get configurations from client properties file
-        Properties prop = new Properties();
-        try{
-            prop.load(new FileInputStream(clientProperties));
-        } catch(IOException e){
-            System.out.println(e);
-        }
-        
-        applyLocking =  Boolean.valueOf(prop.getProperty("applyLocking"));
+        //get the apply locking property from the client properties file
+        properties clientProperties = new propertyhandler(clientProperties);
+        applyLocking = clientProperties.getProperty(applyLocking);
         //initialize structure holding locks
-        locks = new HashMap<>();   
+        locks = new HashMap<Account, Lock>();   
     }
     /*
-    Sets a lock on a transaction
+    Sets a lock on an account
     */
-    public void lock(Transaction transaction, Account account, 
-            LockType lockType){
-        if (!applyLocking){
+    void lock(Account account, Transaction transaction, LockType lockType, applyLocking){
+        
+        Lock found;
+        
+        // prevents race conditions
+        synchronized(this){
+            if (!applyLocking){
             return;
+            }
+            //find the lock associated with the specific account
+            found = locks.get(account);
+            
+            if(found == null){
+               found = new Lock(lock);
+               locks.put(account, found);
+            } 
         }
-        Iterator<Lock> lockIterator = transaction.getLocks().listIterator();
-//        currentLock.acquire(transaction, lockType);
+        found.acquire(transaction, lockType);
     }
-    /*
-    Unlocks a lock on a transaction
+    /* 
+    Unlocks a lock on an account
     */
-    public void unLock(Transaction transaction){
+    void unLock(Transaction transaction){
         if (!applyLocking){
             return;
         }
         Iterator<Lock> lockIterator = transaction.getLocks().listIterator();
         while (lockIterator.hasNext()){
-            Lock currentLock = lockIterator.next();
-            currentLock.release(transaction);
+            Lock currentLock = transaction.getLocks().next();
+            currentLock.realease(transaction);
         }
     }
 }
